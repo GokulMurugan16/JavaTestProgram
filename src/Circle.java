f99eb0d0031b74dc861462781804a49ce71f1a3a
Gokul Murugan <72276642+GokulMurugan16@users.noreply.github.com>
2021-12-08 18:45:07 -0500

    Circle assignment

---
+++ src/Circle.java
@@ -0,0 +1,17 @@
+
+public class Circle {
+	
+	
+	public double area (double radius) {
+		
+		double area = (3.143)*radius*radius; 
+		return area;
+	}
+	
+	public double circumference (double radius) {
+			
+			double circumference = (3.143)*radius*2; 
+			return circumference;
+		}
+
+}
--- src/output.java
+++ src/output.java
@@ -7,56 +7,64 @@ public class output {
 		Scanner input = new Scanner(System.in);
 		AccountInfo a = new AccountInfo();
 		
-		a.pin = 1234;
-		a.balance = 50000.00;
+//		a.pin = 1234;
+//		a.balance = 50000.00;
+//		
+//		System.out.println("Enter Your Account Number: ");
+//		a.accountNumber = input.next();
+//		
+//		System.out.println("Enter Your Account Pin: ");
+//		int pin = input.nextInt();
+//		
+//		if(pin == a.pin) {
+//			
+//			System.out.println("Please Select the transaction :");
+//			System.out.println("1. View Balance");
+//			System.out.println("2. Amount Withdrawl");
+//			System.out.println("3. Amount Deposit");
+//			System.out.println("4. Amount Info");
+//			
+//			switch (input.nextInt()) {
+//			
+//			case 1:
+//				System.out.println("Your Balance is : " + a.balance);
+//				break;
+//			
+//			case 2:
+//				System.out.println("Enter the withdrawl amount");
+//				a.withdrawAmount(input.nextInt());
+//				System.out.println("Your Balance is : " + a.balance);
+//				break;
+//				
+//			case 3:
+//				System.out.println("Enter the Deposit amount");
+//				a.amountAmount(input.nextInt());
+//				System.out.println("Your Balance is : " + a.balance);
+//				break;
+//				
+//			case 4:
+//				System.out.println("Account Number:" + a.accountNumber);
+//				System.out.println("Your Balance is : " + a.balance);
+//				break;
+//				
+//			default:
+//				System.out.println("Please enter a valid choice.");
+//			
+//			}
+//			
+//		}
+//		
+//		else {
+//			System.out.println("Incorrect Pin");
+//		}
+//		
 		
-		System.out.println("Enter Your Account Number: ");
-		a.accountNumber = input.next();
+		System.out.println("Enter the radius of the circle");
+		float radius = input.nextFloat();
+		Circle c = new Circle();
+		System.out.println("Area of the circle : "+ c.area(radius));
+		System.out.println("Circumference of the circle : "+ c.circumference(radius));
 		
-		System.out.println("Enter Your Account Pin: ");
-		int pin = input.nextInt();
-		
-		if(pin == a.pin) {
-			
-			System.out.println("Please Select the transaction :");
-			System.out.println("1. View Balance");
-			System.out.println("2. Amount Withdrawl");
-			System.out.println("3. Amount Deposit");
-			System.out.println("4. Amount Info");
-			
-			switch (input.nextInt()) {
-			
-			case 1:
-				System.out.println("Your Balance is : " + a.balance);
-				break;
-			
-			case 2:
-				System.out.println("Enter the withdrawl amount");
-				a.withdrawAmount(input.nextInt());
-				System.out.println("Your Balance is : " + a.balance);
-				break;
-				
-			case 3:
-				System.out.println("Enter the Deposit amount");
-				a.amountAmount(input.nextInt());
-				System.out.println("Your Balance is : " + a.balance);
-				break;
-				
-			case 4:
-				System.out.println("Account Number:" + a.accountNumber);
-				System.out.println("Your Balance is : " + a.balance);
-				break;
-				
-			default:
-				System.out.println("Please enter a valid choice.");
-			
-			}
-			
-		}
-		
-		else {
-			System.out.println("Incorrect Pin");
-		}
 		
 	}
 
